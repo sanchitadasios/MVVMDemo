512f69be11fba3efda2c351b155828333748da58
SanchitaDas <sanch.frnd@gmail.com>
2024-02-09 11:23:56 +0000

    Initial Commit

---
+++ MVVMDemo/MVVMDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMDemoApp.swift
+//  MVVMDemo
+//
+//  Created by Sanchita Das on 09/02/24.
+//
+
+import SwiftUI
+
+@main
+struct MVVMDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
